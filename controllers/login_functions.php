8163a7093fb7232ffbc594ac50cdee4027e865f1
codewithv1n <vinzelmarano@gmail.com>
2026-03-30 21:10:28 +0800

    UPDATE: i make logic for login of users only in a separate file

---
+++ controllers/login_functions.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+include '../controllers/connect_db.php'; 
+
+$user_input = $_POST['username'];
+$pass_input = $_POST['password'];
+
+$stmt = $conn->prepare("SELECT id, password FROM users WHERE username = ?");
+$stmt->bind_param("s", $user_input); 
+$stmt->execute();
+
+
+$result = $stmt->get_result();
+
+if ($user = $result->fetch_assoc()) {
+    
+    if (password_verify($pass_input, $user['password'])) {
+        
+        $_SESSION['user_id'] = $user['id'];
+        header("Location: ../users/dashboard.php");
+        exit();
+
+    } else {
+        echo "Wrong Password!";
+    }
+
+} else {
+    echo "User not found!";
+}
+
+
+?>
