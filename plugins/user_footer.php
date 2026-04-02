639cd9a4b0df153bfad44c7182990aeb38977e78
codewithv1n <vinzelmarano@gmail.com>
2026-04-02 21:59:26 +0800

    UPDATE: new file upload

---
+++ plugins/user_footer.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/user_footer_design.css">
+</head>
+
+<body>
+ 
+<div class="footer">
+  
+ <i>Find IT 2026</i>
+ <i>Made by codewithv1n</i>
+
+
+</div>
+
+
+</body>
+</html>
