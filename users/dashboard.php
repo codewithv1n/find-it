b404bbafafa11cb9397e281a060ca68d47ca0aa2
codewithv1n <vinzelmarano@gmail.com>
2026-04-03 22:31:35 +0800

    UPDATE: i add recent item card and mock data and also i put user-info cards and notif bell

--- users/dashboard.php
+++ users/dashboard.php
@@ -14,7 +14,8 @@ if (!isset($_SESSION['user_id'])) {
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title>Find-IT - Dashboard</title>
+  <title>Find IT - Dashboard</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="css/user_main_styles.css" />
   <link rel="stylesheet" href="../plugins/css/user_sidebar_design.css">
   <link rel="stylesheet" href="../plugins/css/user_footer_design.css">
@@ -23,7 +24,16 @@ if (!isset($_SESSION['user_id'])) {
 
 <header>
   <h1>FIND IT</h1>
-  <span id="date-display"></span>
+
+<div class="user-info">
+  <i class="fa fa-user-circle-o">
+   <h5></h5>
+  </i>
+</div>
+  
+   <div class="notification-bell">
+     <i class="fa fa-bell"><?php ?></i>
+   </div>
 
   <div class="user-actions">
     <button onclick="logout()" class="logout-btn">Logout</button>
@@ -37,9 +47,50 @@ if (!isset($_SESSION['user_id'])) {
 <div class="container">
 
  <div class="dashboard-card-1">
-  <h1>WELCOME BACK <?php echo '$name','!' ?></h1>
-  <h6>Joined since: <?php echo '$date' ?></h6>
+    <h1>WELCOME BACK,<?php echo 'codewithv1n','!' ?></h1>
+     <div class="dashboard-card1-content" >
+        <span><i class="fa fa-calendar"></i>Thursday, April 2, 2026</span>
+        <span><i class="fa fa-history"></i>Joined since: September 19,2022</span>
+     </div>
  </div>
+ 
+
+
+  <div class="recent-posting-card">
+   <h2 class="recent-header">
+        <i class="fa fa-history"></i> Recent Item Posting
+    </h2>
+     
+    <div class="posted-data-container">
+      <div class="posted-data">
+        <i class="fa fa-user-circle-o"></i><h6>codewithv1n</h6>
+        <p>Descriptions: </p>
+        <p>Location: </p>
+      </div>
+    </div>
+
+    <div class="posted-data-container">
+      <div class="posted-data">
+        <i class="fa fa-user-circle-o"></i><h6>codewithv1n</h6>
+        <p>Descriptions: </p>
+        <p>Location: </p>
+      </div>
+    </div>
+  
+    <div class="posted-data-container">
+      <div class="posted-data">
+        <i class="fa fa-user-circle-o"></i><h6>codewithv1n</h6>
+        <p>Descriptions: </p>
+        <p>Location: </p>
+      </div>
+    </div>
+  
+  
+  </div> 
+  
+
+
+  
 </div>
 
 
